cad46846d3935f970c4c718ae94c1fc2f63de191
Pranav Marathe <pmarathe25@gmail.com>
2017-01-26 14:29:37 -0500

    Updates group allocator.

--- test/test.cpp
+++ test/test.cpp
@@ -6,6 +6,7 @@
 int main() {
     Server test1 = Server(1, 3, 4);
     GroupAllocator allocater = GroupAllocator(3, 3);
+    // Creeate map of unavailable slots.
     std::map<int, std::vector<int> > occupancy;
     occupancy.insert({0, {1, 5}});
     occupancy.insert({1, {0, 3}});

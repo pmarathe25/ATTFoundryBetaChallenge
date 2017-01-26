4f7b564e88a4b6c7e04e5bf8bbc5dd6d48306e64
Pranav Marathe <pmarathe25@gmail.com>
2017-01-26 11:31:32 -0500

    Initial Commit.

--- include/Group/GroupAllocator.hpp
+++ include/Group/GroupAllocator.hpp
@@ -1,9 +1,10 @@
 #include <map>
+#include <vector>
 
 class GroupAllocator {
     public:
         GroupAllocator(int numRacks, int numRacksPerGroup);
-        allocate(std::map<int, int> unavailableSlots);
+        allocate(std::map<int, std::vector<int> > unavailableSlots);
     private:
         int numRacks, numRacksPerGroup, numGroups;
 

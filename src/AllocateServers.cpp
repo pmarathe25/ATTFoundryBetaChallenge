9cc919f0d20af52a180def20fc4a05b6fa4eecb7
Pranav Marathe <pmarathe25@gmail.com>
2017-01-30 14:37:52 -0500

    Fixes Executable flags.

--- src/AllocateServers.cpp
+++ src/AllocateServers.cpp
@@ -2,12 +2,13 @@
 #include "Group/Group.hpp"
 #include "Group/GroupAllocator.hpp"
 #include "Server/ServerAllocator.hpp"
+#include <string>
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         throw std::invalid_argument("Must provide both an input and output file.");
     }
-    ServerAllocator serverAllocator = (argc > 3 && argv[3] == "capacity") ? ServerAllocator(CAPACITY) : ServerAllocator(EFFICIENCY);
+    ServerAllocator serverAllocator = (argc > 3 && toupper(argv[3][0]) == 'C') ? ServerAllocator(CAPACITY) : ServerAllocator(EFFICIENCY);
     serverAllocator.read(argv[1]);
     serverAllocator.allocateServers();
     serverAllocator.write(argv[2]);

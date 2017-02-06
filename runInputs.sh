91f3da71b43cce7a253bafecaf11a90dbb74e8ec
Pranav Marathe <pmarathe25@gmail.com>
2017-02-06 14:21:21 -0500

    Adds shell script for easy execution.

---
+++ runInputs.sh
@@ -0,0 +1,1 @@
+for i in {1..5}; do bin/AllocateServers Inputs/input-$i.in Outputs/output-$i.out $1; done
--- test/test.cpp
+++ test/test.cpp
@@ -5,13 +5,13 @@
 
 int main() {
     ServerAllocator serverAllocator = ServerAllocator(EFFICIENCY);
-    serverAllocator.read("/home/pranav/Downloads/input-1.in");
+    serverAllocator.read("test/testInput.in");
     // serverAllocator.read("test/input");
     serverAllocator.allocateServers();
     // serverAllocator.displayServer(6771);
-    serverAllocator.write("~/Downloads/output-1.out");
+    serverAllocator.write("test/testOutput.out");
     // serverAllocator.displayGroup(6);
-    serverAllocator.displayGroups();
+    // serverAllocator.displayGroups();
     // serverAllocator.write("test/output");
     // serverAllocator.displayGroups();
     // serverAllocator.displayServersByGroup();
